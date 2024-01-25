7fb0083564a64ee82ff2fa6882699013f7a41ce1
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-01-25 17:17:29 -0300

    se añade las funciones y la pagina para registrar usuarios

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, session, redirect, flash, url_for
 from flask_login import login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 from app.forms import LoginForm
 from . import auth
-from app.firestore_service import get_user
+from app.firestore_service import get_user, user_put
 from app.models import UserData, UserModel
 
 @auth.route('/login', methods=['GET', 'POST']) # Indicamos la ruta de la página y los métodos permitidos
@@ -11,40 +12,74 @@ def login():
         flash('Ya estás logueado')
         return redirect(url_for('index'))
 
-    else:
-        # Si no está logeado
-        login_form = LoginForm() 
-        contexto = {
-            'login_form': login_form
-        }
+    # Si no está logeado
+    login_form = LoginForm() 
+    contexto = {
+        'login_form': login_form
+    }
 
-        if login_form.validate_on_submit(): # Si el formulario es válido
-            username = login_form.username.data # Obtenemos el nombre de usuario de la base de datos
-            password = login_form.password.data
+    if login_form.validate_on_submit(): # Si el formulario es válido
+        username = login_form.username.data # Obtenemos el nombre de usuario de la base de datos
+        password = login_form.password.data
 
-            user_doc = get_user(username) # Obtenemos el usuario de la base de datos
+        user_doc = get_user(username) # Obtenemos el usuario de la base de datos
 
-            if user_doc.to_dict() is not None: # Si el usuario existe
-                password_from_db = user_doc.to_dict()['password'] # de los datos del usuario obtenemos la contraseña
+        if user_doc.to_dict() is not None: # Si el usuario existe, validamos sus datos
+            password_from_db = user_doc.to_dict()['password'] # Obtenemos la contraseña de la base de datos
 
-                if password == password_from_db: # Si la contraseña es correcta
-                    user_data = UserData(username, password) # se guarda la data del usuario
-                    user = UserModel(user_data) # Se crea el modelo del usuario que creamos en models.py
+            # if password == password_from_db: # Si la contraseña es correcta # si los usuarios no tuvieran la contraseña encriptada
+            # if check_password_hash(user_doc.to_dict()['password'], password): # si todos los usuarios tuvieran la contraseña encriptada
+            if check_password_hash(user_doc.to_dict()['password'], password) or (password == password_from_db): #Verifica ambas opciones
+                user_data = UserData(username, password)
+                user = UserModel(user_data)
 
-                    login_user(user) # Ese usuario se loguea en nuestra aplicación
+                login_user(user)
 
-                    flash('Bienvenido de nuevo')
+                flash('Bienvenido de nuevo')
 
-                    redirect(url_for('hello'))
-                
-                else:
-                    flash('La información no coincide')
+                return redirect(url_for('hello'))
             else:
-                flash('El usuario no existe')
+                flash('La información no coincide')
+        else:
+            flash('El usuario no existe')
 
-            return redirect(url_for('index'))
+        return redirect(url_for('index'))
 
-        return render_template('login.html', **contexto)
+    return render_template('login.html', **contexto)
+
+
+# Ruta para registrarse
+@auth.route('signup', methods=['GET', 'POST'])
+def signup():
+    signup_form = LoginForm()
+    contexto = {
+        'signup_form': signup_form
+    }
+
+    if signup_form.validate_on_submit():
+        # Primero debemos verificar que el usuario no exista
+        username = signup_form.username.data
+        password = signup_form.password.data
+
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is None: # Si el usuario no existe
+            password_hash = generate_password_hash(password) # Generamos el hash de la contraseña
+            user_data = UserData(username, password_hash)
+            user_put(user_data)
+
+            user = UserModel(user_data)
+
+            login_user(user) # apenas esté registrado pasa de inmediato a la página de hello, con sus datos de logeado
+
+            flash('Bienvenido')
+
+            return redirect(url_for('hello'))
+        else:
+            flash('El usuario ya existe')
+
+    return render_template('signup.html', **contexto)
+
 
 
 # para cerrar sesión
